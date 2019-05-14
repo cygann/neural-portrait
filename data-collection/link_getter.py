ba565e1eb6d7a7576bde01eadf88ab13384baf5e
Priscilla Lui <priscilla.pslui@gmail.com>
2019-05-13 22:37:27 -0700

    Update link_getter.py

--- data-collection/link_getter.py
+++ data-collection/link_getter.py
@@ -5,6 +5,7 @@ links_file = open("image_links.txt","w+")
 with open('catalog.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    written_count = 0
     for row in csv_reader:
         if line_count == 0:
             pass
@@ -14,9 +15,11 @@ with open('catalog.csv') as csv_file:
             unique_substr = html_link[unique_index : -5]
             image_link = 'https://www.wga.hu/detail/' + unique_substr + '.jpg'
             links_file.write(image_link + '\n')
-            artist = row[0]
+            written_count += 1
+
            
         line_count += 1
     print(f'Processed {line_count} lines.')
+    print(f'Wrote {written_count} lines.')
 
 links_file.close()
